0520fc10d248dfa2a00fd062f8dcb777488a0e9d
DeathStorm <Death_Storm@web.de>
2015-06-08 19:17:23 +0200

    h

---
+++ Editor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPGEditor
+{
+    public partial class Editor : Form
+    {
+
+
+        private Dictionary<int, Scene> scenes = new Dictionary<int, Scene>();
+        private int nextSceneNumber = 0;
+
+        private struct Scene
+        {
+            public String title;
+            public String Story;
+        }
+
+
+
+
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            scenes.Add(nextSceneNumber, new Scene());
+            nextSceneNumber++;
+
+        }
+    }
+}
